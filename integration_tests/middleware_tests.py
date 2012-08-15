8e26b7925e40a8125e3422a30c51cfe32b702c42
Colin Rothwell <colin.rothwell@red-gate.com>
2012-08-15 17:44:27 +0100

    Add failing integration test for metadata endpoint

--- integration_tests/middleware_tests.py
+++ integration_tests/middleware_tests.py
@@ -10,8 +10,7 @@ def static_files_are_served_correctly():
     file_names = ['logo.png', 'sprite.png', 'glimpse.js']
 
     for file_name in file_names:
-        url = 'http://localhost:{0}/glimpse/{1}'.format(PORT, file_name)
-        request = requests.get(url)
+        request = requests.get(_resource_url(file_name))
         assert_equal(200, request.status_code)
 
         file_path = dirname(__file__) + '/../static_files/' + file_name
@@ -20,10 +19,21 @@ def static_files_are_served_correctly():
 
 @test
 def script_files_are_inserted():
-    request = requests.get('http://localhost:{0}/'.format(PORT))
+    request = requests.get(_application_url())
     script_tag = '<script type="text/javascript" src="/glimpse/glimpse.js">'
     script_tag += '</script>'
     script_tag += '<script type="text/javascript" '
     script_tag += 'src="/glimpse/metadata?callback=glimpse.data.initMetadata">'
     script_tag += '</script>'
     assert_in(script_tag, request.text)
+
+@test
+def metadata_endpoint_exists():
+    request = requests.get(_resource_url('metadata'))
+    assert_equal(200, request.status_code)
+
+def _resource_url(resource):
+    return _application_url('glimpse/' + resource)
+
+def _application_url(query=''):
+    return 'http://localhost:{0}/{1}'.format(PORT, query)
