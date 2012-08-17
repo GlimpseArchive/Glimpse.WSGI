9965f7a7526b8a1d536d494c1ea5e7d121648176
reviewboard <lewis.brown@red-gate.com>
2012-08-17 14:12:27 +0100

    Test that request resource returns json containing information from environ

--- integration_tests/requestresource_tests.py
+++ integration_tests/requestresource_tests.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 from nose.tools import assert_in, assert_equal
 
@@ -7,10 +8,21 @@ from urls import application_url, resource_url
 
 @test
 def request_resource_returns_json_about_given_request():
+    response = _send_request_to_requestresource()
+
+    assert_equal(200, response.status_code)
+    assert_equal('application/json', response.headers['content-type'])
+
+@test
+def request_resource_json_contains_information_from_environ():
+    response_json = _send_request_to_requestresource().text
+    response = json.loads(response_json)
+    assert_in('data', response)
+    assert_in('REQUEST_METHOD', response['data'])
+
+def _send_request_to_requestresource():
     request = requests.get(application_url())
     assert_in('x-glimpse-requestid', request.headers)
     request_id = request.headers['x-glimpse-requestid']
 
-    request = requests.get(resource_url('request/{}'.format(request_id)))
-    assert_equal(200, request.status_code)
-    assert_equal('application/json', request.headers['content-type'])
+    return requests.get(resource_url('request/{}'.format(request_id)))
